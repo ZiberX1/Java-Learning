0f56416aca974860e82c958bd1966116ba0e3c5a
ZiberX <ziberx01@gmail.com>
2024-03-22 17:35:51 +0700

    Create Sums.java

---
+++ Project/SumTwoNumbers/Sums.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Sums {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        int a = input.nextInt();
+        
+        System.out.println("Enter second number: ");
+        int b = input.nextInt();
+
+        input.close();
+
+        System.out.println("Sum = " + sum(a, b));
+        // Output:
+        /*
+            Enter first number: 
+            100
+            Enter second number:
+            20
+            Sum = 120
+        */
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
